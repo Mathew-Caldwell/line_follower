6e318dd400d990cc3c5533d15c9273f3d08dbd10
Mathew-Caldwell <20143330@tafe.wa.edu.au>
2025-11-26 09:23:37 +0800

    added ps6 sensor subscription

--- line_follower/line_follower_3330.py
+++ line_follower/line_follower_3330.py
@@ -10,6 +10,8 @@ class MinimalSubscriber(Node):
         
         self.subscription = self.create_subscription(Range,'ps7',self.listener_callback,10)
         self.subscription  # prevent unused variable warning
+        self.subscription = self.create_subscription(Range, 'ps6', self.listener_callback, 10)
+        self.subscription
 
         self.publisher_ = self.create_publisher(TwistStamped, 'cmd_vel', 10)
         #timer_period = 0.5  # seconds
@@ -32,9 +34,9 @@ class MinimalSubscriber(Node):
         twist_msg.linear = Vector3(x=0.1, y=0.0, z=0.0)
         twist_msg.angular = Vector3(x=0.0, y=0.0, z=0.0)
 
-        if sensor_range <= 0.063:
-            twist_msg.linear = Vector3(x=0.1, y=0.0, z=0.0)
-            twist_msg.angular = Vector3(x=0.0, y=0.0, z=-0.5)
+        if sensor_range <= 0.0675:
+            twist_msg.linear = Vector3(x=0.001, y=0.0, z=0.0)
+            twist_msg.angular = Vector3(x = 0.0, y = 0.0, z = -0.9)
 
         twist_stamped_msg.twist = twist_msg
         self.publisher_.publish(twist_stamped_msg)
